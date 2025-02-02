4e96a46875b3bfb517e913592d4815ebb3144320
pranavraut9492 <pranavraut9492@gmail.com>
2025-02-02 10:54:36 +0000

    add service discovery code

--- src/main/java/com/project/product/ProductApplication.java
+++ src/main/java/com/project/product/ProductApplication.java
@@ -3,8 +3,10 @@ package com.project.product;
 import com.project.product.factory.ProductCatalogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication(scanBasePackages = "com.project.product")
+@EnableEurekaServer
 public class ProductApplication {
 
 	public static void main(String[] args) {
